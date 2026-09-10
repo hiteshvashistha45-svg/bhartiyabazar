0fd883b69e0a84b8f8c966dc1bd94d71cc2c5e9a
hiteshvashistha45-svg <hiteshvashistha45@gmail.com>
2026-09-10 18:36:53 +0530

    Create themecontext.jsx

---
+++ srs/contexts/themecontext.jsx
@@ -0,0 +1,3 @@
+import { createContext } from "react"
+export const ThemeContext = createContext()
+export function ThemeProvider({ children }) { return <ThemeContext.Provider value={{}}>{children}</ThemeContext.Provider> }
