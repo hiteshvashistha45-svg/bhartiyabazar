3711eed138ccee3976e64d9a11d8f0481bbbf31a
hiteshvashistha45-svg <hiteshvashistha45@gmail.com>
2026-09-13 23:49:15 +0530

    Delete srs directory

--- srs/contexts/themecontext.jsx
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react"
-export const ThemeContext = createContext()
-export function ThemeProvider({ children }) { return <ThemeContext.Provider value={{}}>{children}</ThemeContext.Provider> }
--- srs/pages/referralpage.jsx
+++
@@ -1,1 +0,0 @@
-export default function ReferralPage(){ return <div style={{padding:20}}><h1>Refer & Earn</h1><p>Coming soon</p></div> }
